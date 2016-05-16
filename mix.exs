79257c8e9bbe0478e03a19e93d804283dec256e1
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-05-16 17:31:32 -0600

    release 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fugue.Mixfile do
   def project do
     [app: :fugue,
      description: "Extendable testing utilities for Plug",
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
